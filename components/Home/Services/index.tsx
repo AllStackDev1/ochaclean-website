2bbd342236b12a2cb678fc5d67ed822f78782381
Gigman <abbeykojoeric@gmail.com>
2021-11-23 10:57:54 +0000

    added services component to home

---
+++ components/Home/Services/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+import {
+  Box,
+  Icon,
+  Flex,
+  Grid,
+  Text,
+  GridItem,
+  Image,
+  Link
+} from '@chakra-ui/react'
+
+const Services: FC = () => {
+  return <div></div>
+}
+
+export default Services
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,15 @@ import type { NextPage } from 'next'
 import Layout from '@components/Layout'
 import Hero from '@components/Home/Hero'
 import Review from '@components/Home/Review'
-import AnswerYes from '@components/Home/AnswerYes'
-import Hire from '@components/Home/Hire'
-import CTA from '@components/CTA'
 import Hiw from '@components/Home/Hiw'
+import Services from '@components/Home/Services'
 const Home: NextPage = () => {
   return (
     <Layout>
       <Hero />
       <Hiw />
       <Review />
+      <Services />
     </Layout>
   )
 }

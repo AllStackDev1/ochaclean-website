eff31ef75e2143ae5806f4f7547d8f61740f5e2e
Gigman <abbeykojoeric@gmail.com>
2021-11-23 04:39:36 +0000

    added hiw

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,12 @@ import YourProblems from '@components/Home/YourProblems'
 import AnswerYes from '@components/Home/AnswerYes'
 import Hire from '@components/Home/Hire'
 import CTA from '@components/CTA'
-
+import Hiw from '@components/Home/Hiw'
 const Home: NextPage = () => {
   return (
     <Layout>
       <Hero />
+      <Hiw />
       <YourProblems />
       <AnswerYes />
       <Hire />
